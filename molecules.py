f2c3bbc8d2cf7aee2fde951791e54cf1f9f87085
Gonzague de Carpentier <decarpentierg@gmail.com>
2023-02-15 22:49:43 +0100

    improve Molcule class and molecule drawings

--- molecules.py
+++ molecules.py
@@ -1,32 +1,45 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
+
+
+NODECM = cm.get_cmap('Set1')  # colormap for nodes
+EDGECM = ['red', 'green', 'blue', 'yellow']  # colormap for edges
+
 
 class Molecule(nx.classes.graph.Graph):
     def __init__(self, incoming_graph_data=None, **attr):
         super().__init__(incoming_graph_data, **attr)
+        self.atoms = [(node[0], node[1]['labels'][0]) for node in self.nodes(data=True)]
+        self.bonds = [(edge[0], edge[1], edge[2]['labels'][0]) for edge in self.edges(data=True)]
     
-    def nodelabels(self):
-        nodeview = self.nodes(data=True)
-        labels = [node[1]["labels"][0] for node in nodeview]
-        return np.array(labels)
-    
-    def plot_graph(self):
-        plt.figure()
-        pos= nx.spring_layout(self)
-        edge_to_labels = nx.get_edge_attributes(self,'labels')
-        edge_labels = {e: edge_to_labels[e] for e in nx.get_edge_attributes(self,'labels')}
-        RGB = ["r","g","b"]
-        edge_colors = [RGB[e[2]['labels'][0]] for e in self.edges(data=True)]
+    def plot(self, **kwargs):
+        """
+        Plot molecule as graph.
+        """
+        # Define keyword arguments to pass to function nx.draw
+        default_kwargs = {"width":1, "linewidths":1, "font_size":15, "node_size":500}
+        default_kwargs.update(kwargs)
 
+        # Initialize figure
+        plt.figure()
+
+        # Compute node positions
+        pos = nx.spring_layout(self)
+
+        # Compute node labels
+        node_labels = {atom[0]: atom[1] for atom in self.atoms}
+
+        # Compute node and edge colors
+        node_colors = [NODECM(atom[1]) for atom in self.atoms]
+        edge_colors = [EDGECM[bond[2]] for bond in self.bonds]
+
+        # Draw molecule
         nx.draw(
-            self, pos, width=1, linewidths=1,
-            node_size=500, node_color='yellow', edge_color = edge_colors, alpha=0.8, font_size=15,
-            labels={node[0]: node[1]['labels'][0] for node in self.nodes(data=True)},arrow_size=20
+            self, pos, node_color=node_colors, edge_color=edge_colors, labels=node_labels, **default_kwargs
         )
 
+        # Show plot
         plt.axis('off')
         plt.show()
-
-
-
